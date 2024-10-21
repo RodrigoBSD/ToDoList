1e060abbe4a2a549e843f21e019922bbda8e1b94
Rodrigo Brasiliense <64172938+RodrigoBSD@users.noreply.github.com>
2024-10-21 01:24:38 -0300

    Update TarefaEditorViewModel.cs

--- ToDoListAPI/ViewModel/TarefaEditorViewModel.cs
+++ ToDoListAPI/ViewModel/TarefaEditorViewModel.cs
@@ -6,12 +6,8 @@ namespace ToDoListAPI.ViewModel
     public class TarefaEditorViewModel
     {
 
-        [Required(ErrorMessage = " Titulo é obrigatorio")]
-        [MaxLength(100, ErrorMessage = "Titulo não pode ter mais de 100 caracteres")]
         public string Titulo { get; set; }
 
-        [Required(ErrorMessage = " Descrição é obrigatorio")]
-        [MaxLength(350, ErrorMessage = "Descrição não pode ter mais de 350 caracteres")]
         public string Descricao { get; set; }
 
         public Status Status { get; set; }
